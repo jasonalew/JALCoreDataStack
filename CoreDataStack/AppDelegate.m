1f806cf3cec965b62c6d07461548716cfe3e0fb0
Jason Lew <jason.a.lew@gmail.com>
2015-08-22 21:38:22 -0700

    Updated Core Data stack initializer and saved on exit.

--- CoreDataStack/AppDelegate.m
+++ CoreDataStack/AppDelegate.m
@@ -22,7 +22,11 @@
     // Override point for customization after application launch.
     
     // Create the Core Data stack
-    self.coreDataStack = [[JALCoreDataStack alloc]init];
+    NSDictionary *options = @{NSMigratePersistentStoresAutomaticallyOption: @YES,
+                              NSInferMappingModelAutomaticallyOption: @YES};
+    self.coreDataStack = [[JALCoreDataStack alloc]initWithStoreType:NSSQLiteStoreType
+                                                              model:@"Recipes"
+                                                            options:options];
     
     // Get the main tableVC and set its managed object context
     UINavigationController *navVC = (UINavigationController *)self.window.rootViewController;
@@ -40,6 +44,7 @@
 - (void)applicationDidEnterBackground:(UIApplication *)application {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    [self.coreDataStack saveContext:YES];
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
@@ -52,6 +57,7 @@
 
 - (void)applicationWillTerminate:(UIApplication *)application {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    [self.coreDataStack saveContext:YES];
 }
 
 @end
